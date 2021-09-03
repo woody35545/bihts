61c85a3f662263f6688d719dab01bc2d38e2d2b5
woody <woody35545@gmail.com>
2021-09-03 21:48:17 +0900

    add simple gui

--- gui/gui_classes.py
+++ gui/gui_classes.py
@@ -7,6 +7,15 @@ class main_window_class(QMainWindow, main_ui_form) :
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
+        self.btn_getCurrentPrice.clicked.connect(self.action_btn_getCurrentPrice)
+        self.btn_command.clicked.connect(self.action_btn_command)
+    def action_btn_getCurrentPrice(self):
+        self.te_console.append("1800 won")
+    def action_btn_command(self):
+        input_command_ = (self.te_command.toPlainText())
+        self.te_console.append(input_command_)
+
+        self.te_command.clear()
 
 def init_windows():
     app = QApplication(sys.argv)
